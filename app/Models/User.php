ee1106c27c821a1894df533f381d946e098e96e6
Phạm Quang Linh <phamquanglinhdev@gmail.com>
2023-03-31 03:06:36 +0700

    ad

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,7 +60,7 @@ class User extends Authenticatable
 
     public function hasRole(string $role): bool
     {
-        if ($this->role = "super") {
+        if ($this->id == 1) {
             return true;
         }
         if ($this->permissions != null) {
